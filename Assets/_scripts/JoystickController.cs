deac5e9b06f1a6fcc171bf24956a520478e8a446
Mykola Niunko <mukolaniunko@gmail.com>
2022-12-02 16:32:28 +0200

    .

---
+++ Assets/_scripts/JoystickController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof (CapsuleCollider))]
+
+public class JoystickController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private float _moveSpeed;
+
+    private void FixedUpdate()
+    {
+      _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed); 
+
+      if(_joystick.Horizontal !=0 || _joystick.Vertical !=0)
+      {
+        transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+      } 
+    }
+
+
+}
